d0b41dde95bccc467cd15e44d43f0389a69d32c6
Aldo Vilardy <aldovilardy@gmail.com>
2019-08-07 15:07:28 -0500

    Xamarin Project
    Initial commit xamarin project

---
+++ PaniniStickerClientApp/PaniniStickerClientApp/PaniniStickerClientApp/Views/MediaPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PaniniStickerClientApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MediaPage : ContentPage
+    {
+        public MediaPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PickPhoto_Clicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.PickPhotoAsync(
+                new PickMediaOptions
+                {
+                    PhotoSize = PhotoSize.Medium
+                });
+
+            if (file == null) return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                Stream stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        private async void TakePhoto_Clicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(
+                new StoreCameraMediaOptions
+                {
+                    Directory = "Test",
+                    SaveToAlbum = true,
+                    CompressionQuality = 75,
+                    CustomPhotoSize = 50,
+                    PhotoSize = PhotoSize.MaxWidthHeight,
+                    MaxWidthHeight = 2000,
+                    DefaultCamera = CameraDevice.Front
+                });
+
+            if (file == null) return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                Stream stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+    }
+}
