2c2b078fcff44d47c7c87d2a2a5cd79406c18978
aldovilardy <aldovilardy@gmail.com>
2019-09-18 23:21:42 -0500

    Define basic project structure

---
+++ PaniniStickerClientApp/PaniniStickerClientApp/PaniniStickerClientApp/ViewModels/FormViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PaniniStickerClientApp.ViewModels
+{
+    public class FormViewModel
+    {
+        public string Frame { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public DateTime DoB { get; set; }
+        public string Club { get; set; }
+        public string Debut { get; set; }
+        public ICommand TakePhotoCommand => new RelayCommand(TakePhoto);
+        public ICommand PickPhotoCommand => new RelayCommand(PickPhoto);
+        public ImageSource PhotoPreview { get; set; }
+
+        private void TakePhoto()
+        {
+            throw new NotImplementedException();
+        }
+        private void PickPhoto()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
